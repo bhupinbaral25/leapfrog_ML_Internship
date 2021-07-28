48842bb933f50aa69393cefd32bbf4873566f8f5
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-07-28 11:07:28 +0545

    API creation with ML model

---
+++ my_api.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+from flask import Flask , request
+from joblib import load
+import numpy as np
+
+app = Flask(__name__)
+model = load('exam.joblib') 
+
+@app.route("/Predict/", methods=['GET'])
+def main():
+    y = model.predict(np.array(2).reshape(-1, 1))
+    return y
+
+if __name__=='__main__':
+    app.run(debug=True)
