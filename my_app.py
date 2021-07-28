bd766efe2f7444470992b698987bc15b3f7c1745
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-07-28 16:28:41 +0545

    APP with line chart in streamlit

--- my_app.py
+++ my_app.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 import time
 st.title('My first app')
+
 df = pd.read_csv('training_data.csv',sep=':',delimiter=None)
 df
 st.write("This is a  simple line chart for the training data")
 st.line_chart(df)
+st.vega_lite_chart(df)
 
 
 latest_iteration = st.empty()
@@ -17,3 +19,5 @@ for i in range(100):
   latest_iteration.text(f'Iteration {i+1}')
   bar.progress(i + 1)
   time.sleep(1)
+
+  
