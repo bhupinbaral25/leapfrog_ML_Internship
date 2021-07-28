3a66642bb2d61c7b00884b620d7067aa70ed4b09
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-07-28 14:15:03 +0545

    API created with ML,return using json

--- my_api.py
+++ my_api.py
@@ -1,15 +1,20 @@
 
-from flask import Flask , request
+from flask import Flask ,request, jsonify
 from joblib import load
 import numpy as np
 
 app = Flask(__name__)
 model = load('exam.joblib') 
 
-@app.route("/Predict/", methods=['GET'])
-def main():
-    y = model.predict(np.array(2).reshape(-1, 1))
-    return y
+@app.route("/Predict/<int:values>", methods=['GET'])
+def main(values):
+    y = model.predict(np.array(values).reshape(-1, 1))
+    print(y)
+    results={ 
+            "Results for your values": y,
+            "given value":values
+    }
+    return jsonify(results)
 
 if __name__=='__main__':
     app.run(debug=True)
